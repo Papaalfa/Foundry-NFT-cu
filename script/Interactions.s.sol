127478ad13f64fe5d035dfe1a1bdf3d087737d13
Papaalfa <s@zimin.me>
2026-01-09 00:17:24 +0100

    Added scripts and tests + some minor fixes

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -5,23 +5,57 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNft.sol";
+import {MoodNft} from "../src/MoodNft.sol";
 
 contract MintBasicNft is Script {
     string public constant PUG =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
-            "BasicNft",
-            block.chainid
-        );
+        address mostRecentlyDeployedBasic = DevOpsTools
+            .get_most_recent_deployment("BasicNft", block.chainid);
 
-        mintNftOnContract(mostRecentlyDeployed);
+        mintNftOnContract(mostRecentlyDeployedBasic);
     }
 
     function mintNftOnContract(address contractAddress) public {
         vm.startBroadcast();
         BasicNft(contractAddress).mintNft(PUG);
+        vm.stopBroadcast();
+    }
+}
+
+contract MintMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployedMood = DevOpsTools
+            .get_most_recent_deployment("MoodNft", block.chainid);
+
+        mintNftOnContract(mostRecentlyDeployedMood);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        MoodNft(contractAddress).mintNft();
+        vm.stopBroadcast();
+    }
+}
+
+contract FlipMoodNft is Script {
+    uint256 public tokenId = vm.envUint("TOKEN_ID");
+
+    function run() external {
+        address mostRecentlyDeployedMood = DevOpsTools
+            .get_most_recent_deployment("MoodNft", block.chainid);
+
+        flipMoodNftOnContract(mostRecentlyDeployedMood, tokenId);
+    }
+
+    function flipMoodNftOnContract(
+        address contractAddress,
+        uint256 tokenId2flip
+    ) public {
+        vm.startBroadcast();
+        MoodNft(contractAddress).flipMood(tokenId2flip);
         vm.stopBroadcast();
     }
 }
